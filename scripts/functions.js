12c8c783df827ad6afc7c486ecb6749cd07f9fd2
Victor Perez Domenech <victorrperez25@gmail.com>
2022-09-20 12:10:10 +0200

    [ADD] ejercicio frase completado

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,31 +1,44 @@
 'use strict'
 
 function letras(cadena) {
-
+	return cadena.length;
 }
 
 function palabras(cadena) {
-
+	let palabrasFrase = cadena.split(' ');
+	return palabrasFrase.length;
 }
 
 function maysc(cadena) {
-
+	return cadena.toLocaleUpperCase();
 }
 
 function titulo(cadena) {
-
+	let palabrasFrase = cadena.split(' ');
+	let cadenaMod = [];
+	for (let palabra of palabrasFrase) {
+		let palabraMayuscula = palabra.charAt(0).toLocaleUpperCase();
+		cadenaMod.push(palabraMayuscula + palabra.substring(1));
+	}
+	let cadenaPrimeraMayuscula = cadenaMod.join(' ');
+	return cadenaPrimeraMayuscula;
 }
 
 function letrasReves(cadena) {
-
+	let cadenaReves = cadena.split('').reverse().join('');
+	return cadenaReves;
 }
 
 function palabrasReves(cadena) {
-
+	let cadenaSeparada = cadena.split(' ');
+	let cadenaPalabrasReves = cadenaSeparada.reverse().join(' ');
+	return cadenaPalabrasReves;
 }
 
 function palindromo(cadena) {
-
+	let cadenaMinuscula = cadena.toLowerCase().split(' ').join('');
+	let cadenaReversa = cadena.replace(/ /g, "").split('').reverse().join('').toLowerCase();
+	return cadenaReversa === cadenaMinuscula;
 }
 
 module.exports = {
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,19 @@
 'use strict'
 
+let fraseUsuario = prompt('Introduce una frase: ');
 
+console.log(letras(fraseUsuario) + ' letras' + ' y ' + palabras(fraseUsuario) + ' palabras.');
+
+console.log(maysc(fraseUsuario));
+
+console.log(titulo(fraseUsuario));
+
+console.log(letrasReves(fraseUsuario));
+
+console.log(palabrasReves(fraseUsuario));
+
+if(palindromo(fraseUsuario)) {
+    console.log("Sí es palíndroma")
+} else {
+    console.log("No es palíndroma");
+}
